5478ddc1ba06f193be0c9e542939033ac35223fa
REDDY PRO <22135a0508@gvpce.ac.in>
2024-11-12 07:35:47 +0530

    Todos pushing updated

--- routes/todos.js
+++ routes/todos.js
@@ -1,76 +1,74 @@
-const express = require('express')
-
-const {todo} = require('../models/todo_model');
+const express = require("express")
 
 const router = express.Router()
 
+const day = require('../models/day_model')
+const { default: mongoose } = require("mongoose")
+
 router.get('/',(req,res)=>{
-    try{
-        res.json({ message : "Todo Route"})
-    }catch(err){
-        res.json({ Error : err})
-    }
+    res.json({ message : "Day Router" })
 })
 
-router.get('/get-todos/:id',async (req,res)=>{
+router.get('/check-day/:id',async(req,res)=>{
+
+    console.log("at check day")
+
     const uid = req.params.id
+    const { date } = req.query
+
+    console.log("Date",date)
+    console.log("uid",uid)
     try{
-     const fetchedTodos = await todo.find({ uid})
-     res.json({ todos : fetchedTodos })
+        const isExists =await day.findOne({ uid , date : date })
+        console.log("isExists",isExists)
+        if(isExists!==null){
+            res.json({ message : "Day alredy exists", flag : true })
+        }else{
+            res.json({ message : "Day Not exists", flag : false })
+        }
     }catch(err){
         res.json({ Error : err})
     }
 })
 
-router.post('/create-todo',async (req,res)=>{
-    const todoObject = req.body.todo
-    console.log(todoObject)
+
+router.post('/create-day',async(req,res)=>{
+    const dayObject = req.body
     try{
-        const newTodoObject = new todo(todoObject)
-        await newTodoObject.save()
-        res.json({ message : "Todo Added" , newTodoObject , flag : true  })
-    }catch(err){
-        console.log("Error creating todo:", err); // Log the error for more details
-        res.status(500).json({
-            message: "Internal server error",
-            error: err.message,
-            stack: err.stack
-        });
-    }
-})
-
-router.patch('/check-todo/:id',async (req,res)=>{
-    const id=  req.params.id
-    const { completed , status } = req.body
-    const updateFields = {}
-    if (completed!==undefined) updateFields.completed = completed
-    if (status !==undefined) updateFields.status = status
-    try{
-        const todoObject = await todo.findOneAndUpdate({ id }, { $set : updateFields }, { new: true })
-
-        res.json({ todoObject , flag : true})
-
-    }catch(err){
-        res.json({ Error : err })
-    }
-})
-
-router.delete('/delete-todo/:id', async (req, res) => {
-    const id = req.params.id;
-    try {
-        const response = await todo.findOneAndDelete({ id });
-
-        if (response) {
-            res.json({ message: "Todo deleted successfully", deleted: true });
-        } else {
-            res.json({ message: "Todo not found", deleted: false });
+        const isExists = await day.findOne({ uid : dayObject.uid , date : dayObject.date })
+        if(!isExists){
+            const createdDayObject = new day(dayObject)
+            await createdDayObject.save()
+            res.json({ message : "Day created" , createdDayObject , flag : true })
+        }else{
+            res.json({ message : "Day Already exists" , dayObject })
         }
-    } catch (err) {
-        res.json({ Error: err });
+    }catch(err){
+        res.json({ Error : err})
     }
-});
+})
 
 
+router.post("/push-day-id/:id", async (req, res) => {
+
+    console.log("Push day called in Days")
+
+  const userId = req.params.id
+  const { todoId , dayDate } = req.body
+
+  const todoObjectId = new mongoose.Types.ObjectId(todoId)
+
+  try {
+    const updatedTodo = await day.findOneAndUpdate(
+      { uid: userId , date : dayDate },
+      { $push: { "todos": todoObjectId } },
+      { new : true }
+    )
+    res.json({ updatedTodo })
+  } catch (err) {
+    res.json({ Error: err })
+  }
+})
 
 
 module.exports = router
